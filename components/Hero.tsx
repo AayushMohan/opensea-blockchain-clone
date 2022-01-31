63c52264eea92b1c86bbe08d9c41d1cbb3a835ce
AayushMohan <aayushmohan1702@gmail.com>
2022-01-31 22:25:19 +0530

    Added Author Details in Hero Component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -46,6 +46,15 @@ const Hero = () => {
                 src="https://lh3.googleusercontent.com/qQj55gGIWmT1EnMmGQBNUpIaj0qTyg4YZSQ2ymJVvwr_mXXjuFiHJG9d3MRgj5DVgyLa69u8Tq9ijSm_stsph8YmIJlJQ1e7n6xj=s64"
                 alt=""
               />
+              <div className={style.author}>
+                <div className={style.name}>Jolly</div>
+                <a
+                  className="text-[#1868b7]"
+                  href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/2324922113504035910649522729980423429926362207300810036887725141691069366277"
+                >
+                  hola-kanola
+                </a>
+              </div>
             </div>
           </div>
         </div>
