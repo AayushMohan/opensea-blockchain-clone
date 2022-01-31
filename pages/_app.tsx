559f0b19f949f4b39a838ae9c786df984efed02b
AayushMohan <aayushmohan1702@gmail.com>
2022-02-01 01:57:11 +0530

    Added Thirdweb Connector hooks in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
+
+/**
+ * The chai ID 4 represents the Rinkeby network.
+ * The `injected` connector is a web3 connection method used by Metamask
+ */
+const supportedChainIds = [4]
+const connector = {
+  injected: {},
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
