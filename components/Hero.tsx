0baa8cea0a978976ade2f51157f474c14e1f5dff
AayushMohan <aayushmohan1702@gmail.com>
2022-01-31 22:20:14 +0530

    Added Info Container In Hero Component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -39,6 +39,14 @@ const Hero = () => {
               className="rounded-t-lg"
               src="https://lh3.googleusercontent.com/ujepnqpnL0nDQIHsWxlCXzyw4pf01yjz1Jmb4kAQHumJAPrSEj0-e3ABMZlZ1HEpJoqwOcY_kgnuJGzfXbd2Tijri66GXUtfN2MXQA=s550"
             />
+
+            <div className={style.infoContainer}>
+              <img
+                className="h-[2.25rem] rounded-full"
+                src="https://lh3.googleusercontent.com/qQj55gGIWmT1EnMmGQBNUpIaj0qTyg4YZSQ2ymJVvwr_mXXjuFiHJG9d3MRgj5DVgyLa69u8Tq9ijSm_stsph8YmIJlJQ1e7n6xj=s64"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
