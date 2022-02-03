19c920aa303d6bc64b0f47a4425b478b3d88521a
AayushMohan <aayushmohan1702@gmail.com>
2022-02-03 22:03:45 +0530

    Added Price Tag

--- components/NFTCard.tsx
+++ components/NFTCard.tsx
@@ -49,7 +49,21 @@ const NFTCard = ({ nftItem, title, listings }) => {
       <div className={style.details}>
         <div className={style.info}>
           <div className={style.collectionName}>{title}</div>
+          <div className={style.assetName}>{nftItem.name}</div>
         </div>
+        {isListed && (
+          <div className={style.infoRight}>
+            <div className={style.priceTag}>Price</div>
+            <div className={style.priceValue}>
+              <img
+                src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg"
+                alt="eth"
+                className={style.ethLogo}
+              />
+              {price}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
