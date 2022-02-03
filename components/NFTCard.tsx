51ffcb867a2cf454a19dda98f3ca483d9dd6c3dc
AayushMohan <aayushmohan1702@gmail.com>
2022-02-03 22:10:15 +0530

    Added Likes

--- components/NFTCard.tsx
+++ components/NFTCard.tsx
@@ -48,22 +48,30 @@ const NFTCard = ({ nftItem, title, listings }) => {
       </div>
       <div className={style.details}>
         <div className={style.info}>
-          <div className={style.collectionName}>{title}</div>
-          <div className={style.assetName}>{nftItem.name}</div>
-        </div>
-        {isListed && (
-          <div className={style.infoRight}>
-            <div className={style.priceTag}>Price</div>
-            <div className={style.priceValue}>
-              <img
-                src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg"
-                alt="eth"
-                className={style.ethLogo}
-              />
-              {price}
-            </div>
+          <div className={style.infoLeft}>
+            <div className={style.collectionName}>{title}</div>
+            <div className={style.assetName}>{nftItem.name}</div>
           </div>
-        )}
+          {isListed && (
+            <div className={style.infoRight}>
+              <div className={style.priceTag}>Price</div>
+              <div className={style.priceValue}>
+                <img
+                  src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg"
+                  alt="eth"
+                  className={style.ethLogo}
+                />
+                {price}
+              </div>
+            </div>
+          )}
+        </div>
+        <div className={style.likes}>
+          <span className={style.likeIcon}>
+            <BiHeart />
+          </span>{' '}
+          {nftItem.likes}
+        </div>
       </div>
     </div>
   )
