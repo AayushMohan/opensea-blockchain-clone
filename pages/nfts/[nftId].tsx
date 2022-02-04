c88f2ef9bb9d8220b5d935665cfdaaa72a000ee5
AayushMohan <aayushmohan1702@gmail.com>
2022-02-04 17:04:56 +0530

    Added Styles section

--- pages/nfts/[nftId].tsx
+++ pages/nfts/[nftId].tsx
@@ -1,12 +1,38 @@
-import React from 'react'
 import Header from '../../components/Header'
 import { useEffect, useMemo, useState } from 'react'
 import { useWeb3 } from '@3rdweb/hooks'
 import { ThirdwebSDK } from '@3rdweb/sdk'
 import { useRouter } from 'next/router'
 
-const Nft = () => {
-  return <Header />
+const style = () => {
+  wrapper: `flex flex-col items-center container-lg text-[#e5e8eb]`,
+  container: `container p-6`,
+  topContainer: `flex`,
+  nftImgContainer: `flex-1 mr-4`,
+  detailsContainer: `flex-[2] ml-4`,
 }
 
-export default Nft
+const NFT = () => {
+  const { provider } = useWeb3() 
+  const [selectedNft, setSelectedNft] = useState()
+  const [listings, setListings] = useState()
+  const router = useRouter()
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  return <Header/>
+}
+
+
+export default NFT
