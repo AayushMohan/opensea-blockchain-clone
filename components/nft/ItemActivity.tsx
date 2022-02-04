f491bdf329e8094e3267e12e6db28967de51b8f1
AayushMohan <aayushmohan1702@gmail.com>
2022-02-04 20:23:28 +0530

    Added Item Activity Component

---
+++ components/nft/ItemActivity.tsx
@@ -0,0 +1,9 @@
+import { CgArrowsExchangeV } from 'react-icons/cg'
+import { AiOutlineDown, AiOutlineUp } from 'react-icons/ai'
+import { useState } from 'react'
+
+const ItemActivity = () => {
+  return <div></div>
+}
+
+export default ItemActivity
