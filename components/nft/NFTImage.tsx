16a4630d1b445b591c925e1c01b06c1d47f5ffa5
AayushMohan <aayushmohan1702@gmail.com>
2022-02-04 17:54:05 +0530

    Added NFT Image Component

---
+++ components/nft/NFTImage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NFTImage = ({ selectedNft }) => {
+  return <h2>Yoo</h2>
+}
+
+export default NFTImage
--- pages/nfts/[nftId].tsx
+++ pages/nfts/[nftId].tsx
@@ -3,8 +3,9 @@ import { useEffect, useMemo, useState } from 'react'
 import { useWeb3 } from '@3rdweb/hooks'
 import { ThirdwebSDK } from '@3rdweb/sdk'
 import { useRouter } from 'next/router'
+import NFTImage from '../../components/nft/NFTImage'
 
-const style = () => {
+const style = {
   wrapper: `flex flex-col items-center container-lg text-[#e5e8eb]`,
   container: `container p-6`,
   topContent: `flex`,
@@ -13,17 +14,17 @@ const style = () => {
 }
 
 const Nft = () => {
-  const { provider } = useWeb3() 
+  const { provider } = useWeb3()
   const [selectedNft, setSelectedNft] = useState()
   const [listings, setListings] = useState()
   const router = useRouter()
 
   const nftModule = useMemo(() => {
     if (!provider) return
-    
+
     const sdk = new ThirdwebSDK(
-    provider?.getSigner(),
-    'https://eth-rinkeby.alchemyapi.io/v2/rljYzpFHeHk60JLsmEkZMwjdnUzoHznO',
+      provider?.getSigner(),
+      'https://eth-rinkeby.alchemyapi.io/v2/rljYzpFHeHk60JLsmEkZMwjdnUzoHznO'
     )
     return sdk.getNFTModule('0xF9a55237B4Ffc9D0C8F466AE59E30Cc0A3084d18')
   }, [provider])
@@ -33,41 +34,41 @@ const Nft = () => {
     if (!nftModule) return
     ;(async () => {
       const nfts = await nftModule.getAll()
-      
+
       const selectedNftArray = nfts.find(
         (nft) => nft.id === router.query.assetId
       )
-      
+
       setSelectedNft(selectedNftArray)
     })()
-    
-  },[nftModule])
-  
-  const marketPlaceModule = useMemo(() => {
+  }, [nftModule])
 
-    if(!provider) return
+  const marketPlaceModule = useMemo(() => {
+    if (!provider) return
 
     const sdk = new ThirdwebSDK(
       provider?.getSigner(),
-      'https://eth-rinkeby.alchemyapi.io/v2/rljYzpFHeHk60JLsmEkZMwjdnUzoHznO',
+      'https://eth-rinkeby.alchemyapi.io/v2/rljYzpFHeHk60JLsmEkZMwjdnUzoHznO'
     )
 
     return sdk.getMarketplaceModule(
       '0x70876A94f84bCD095D3750b230C7fCB27cB50938'
-    ) 
-
+    )
   }, [provider])
-  
+
   useEffect(() => {
     if (!marketPlaceModule) return
     ;(async () => {
       setListings(await marketPlaceModule.getAllListings())
     })()
-
   }, [marketPlaceModule])
 
-  return <Header/>
+  return (
+    <div>
+      <Header />
+      <NFTImage />
+    </div>
+  )
 }
 
-
-export default Nft;
+export default Nft
