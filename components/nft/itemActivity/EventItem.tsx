f55b987ac0e5cf46b13090f85e2396157435ff15
AayushMohan <aayushmohan1702@gmail.com>
2022-02-04 21:18:46 +0530

    Added Style Event Item, Event Icon, Event Name, Event Price, Event Price Value

--- components/nft/itemActivity/EventItem.tsx
+++ components/nft/itemActivity/EventItem.tsx
@@ -13,8 +13,21 @@ const style = {
 
 const EventItem = ({ event }) => {
   return (
-    <div className={style.eventName}>
-      <div className={`${style.event} flex-[2]`}></div>
+    <div className={style.eventItem}>
+      <div className={`${style.event} flex-[2]`}>
+        <div className={style.eventIcon}>
+          <BsFillCartFill />
+        </div>
+        <div className={style.eventName}>Sale</div>
+      </div>
+      <div className={`${style.eventPrice} flex-[2]`}>
+        <img
+          src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg"
+          alt="eth"
+          className={style.ethLogo}
+        />
+        <div className={style.eventPriceValue}>{event.price}</div>
+      </div>
     </div>
   )
 }
