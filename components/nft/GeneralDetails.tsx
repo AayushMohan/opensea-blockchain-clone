873709a17d2834e773d14d00aee406a0c5f25484
AayushMohan <aayushmohan1702@gmail.com>
2022-02-04 18:27:40 +0530

    Imported Icons in General Details Components

--- components/nft/GeneralDetails.tsx
+++ components/nft/GeneralDetails.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import { AiFillHeart } from 'react-icons/ai'
+import { MdRefresh } from 'react-icons/md'
+import { RiShareBoxLine } from 'react-icons/ri'
+import { FiMoreVertical } from 'react-icons/fi'
+import { GiShare } from 'react-icons/gi'
 
 const GeneralDetails = (selectedNft) => {
   return <div></div>
