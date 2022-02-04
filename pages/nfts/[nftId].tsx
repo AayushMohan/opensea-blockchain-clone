3a9de113e51a7664f7d17a5bd4de4f954da815e1
AayushMohan <aayushmohan1702@gmail.com>
2022-02-04 16:47:29 +0530

    Imported 3rd web hooks

--- pages/nfts/[nftId].tsx
+++ pages/nfts/[nftId].tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Header from '../../components/Header'
+import { useEffect, useMemo, useState } from 'react'
+import { useWeb3 } from '@3rdweb/hooks'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+import { useRouter } from 'next/router'
 
 const Nft = () => {
   return <Header />
