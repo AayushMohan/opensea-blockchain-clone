8424b0648ca7401b2ef3fd0673885848a1fcf1f4
AayushMohan <aayushmohan1702@gmail.com>
2022-02-04 21:11:16 +0530

    Added Style

--- components/nft/itemActivity/EventItem.tsx
+++ components/nft/itemActivity/EventItem.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { BsFillCartFill } from 'react-icons/bs'
 
-const EventItem = () => {
+const style = {
+  eventItem: `flex px-4 py-5 font-medium`,
+  event: `flex items-center`,
+  eventIcon: `mr-2 text-xl`,
+  eventName: `text-lg font-semibold`,
+  eventPrice: `flex items-center`,
+  eventPriceValue: `text-lg`,
+  ethLogo: `h-5 mr-2`,
+  accent: `text-[#2081e2]`,
+}
+
+const EventItem = ({ event }) => {
   return <div></div>
 }
 
