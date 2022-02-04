a4e9123516ca08026b608dc5069fb865ed15e3b7
AayushMohan <aayushmohan1702@gmail.com>
2022-02-04 21:20:13 +0530

    Added Style Accent

--- components/nft/itemActivity/EventItem.tsx
+++ components/nft/itemActivity/EventItem.tsx
@@ -28,6 +28,9 @@ const EventItem = ({ event }) => {
         />
         <div className={style.eventPriceValue}>{event.price}</div>
       </div>
+      <div className={`${style.accent} flex-[3]`}>{event.from}</div>
+      <div className={`${style.accent} flex-[3]`}>{event.to}</div>
+      <div className={`${style.accent} flex-[2]`}>{event.date}</div>
     </div>
   )
 }
