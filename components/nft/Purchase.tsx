570ae37b977fb15b1cf0773ef5304da1f478656f
AayushMohan <aayushmohan1702@gmail.com>
2022-02-04 21:34:09 +0530

    Added Style

--- components/nft/Purchase.tsx
+++ components/nft/Purchase.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { HiTag } from 'react-icons/hi'
+import { IoMdWallet } from 'react-icons/io'
+import toast, { Toaster } from 'react-hot-toast'
 
-const Purchase = () => {
-  return <div></div>
+const style = {
+  button: `mr-8 flex items-center py-2 px-12 rounded-lg cursor-pointer`,
+  buttonIcon: `text-xl`,
+  buttonText: `ml-2 text-lg font-semibold`,
 }
-
-export default Purchase
